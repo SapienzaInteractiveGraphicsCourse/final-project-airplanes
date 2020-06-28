66920ecd166be3ef7baf266923a9e812099d0e85
PietroManganelliConforti <manganelliconforti.1754825@studenti.uniroma1.it>
2020-06-29 00:17:04 +0200

    test audio on githubpage

--- audio.js
+++ audio.js
@@ -1,4 +1,4 @@
-
+/*
 var listener = new THREE.AudioListener();
 
 var sound = new THREE.Audio( listener );
@@ -10,14 +10,17 @@ function( buffer ) {
 	sound.setLoop( true );
 	sound.setVolume( 0.5 );
 });
+*/
+var BG_audio = new Audio("https://sapienzainteractivegraphicscourse.github.io/final-project-airplanes/audio/BigCarTheft.mp3");
+
 
 function play(){
 
-    document.getElementById('audio').play();
+    BG_audio.play();
 
     //sound.play();
 
-    //document.getElementById("audio").loop = true;
+    BG_audio.loop = true;
 
     document.getElementById("playB").style.background = "#f52b2b";
     document.getElementById("stopB").style.background = "#006680";
@@ -26,8 +29,9 @@ function play(){
 
   function stop_music(){
 
-  sound.pause();
+  //sound.pause();
 
+  BG_audio.pause();
   document.getElementById("playB").style.background = "#006680";
   document.getElementById("stopB").style.background = "#f52b2b";
 
